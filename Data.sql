4cb0c4aac6c1122c6369db145623a2b22480d6ea
Eva Qi Tian <qi.tian0123@gmail.com>
2020-06-05 10:18:12 -0400

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -19,7 +19,35 @@ SELECT
     ON a.user_id = f.user_id
     LEFT JOIN school_t g
     ON f.id = g.id
+LIMIT 1000  ;
+
+************************************* user_problem_t userful columns **************************************
+select
+  user_id, skill_id, problem_id, problem_type, problem_status, tolerance, score, high_score, accuracy_score, 
+  completion_score, correction_score, correction_high_score, user_attempts, access, insert_dtm
+from user_problem_t
+limit 1000
+  
+
+
+SELECT
+  a.*, 
+  b.*
+    FROM user_problem_t as a
+    LEFT JOIN school_payment_t  as b
+    ON a.user_id = b.user_id
 LIMIT 1000  
+
+SELECT
+  a.*, 
+  b.*
+    FROM user_problem_t as a
+    LEFT JOIN school_payment_t  as b
+    ON a.user_id = b.user_id
+WHERE a.user_id IN (SELECT user_id FROM user_role_t WHERE role_id = 2)
+LIMIT 1000  
+ORDER BY a.user_id, problem_id
+
 
 ***************************** user_id
 *** a
@@ -32,7 +60,7 @@ school_payment_t
 user_skill_t
   user_id, skill_id...
 *** d  
-user_login_t (class_code is important to identify pair)
+user_login_t (class_code and teachers' id are the same things)
   user_id, class_code
 *** e
 user_level_t
